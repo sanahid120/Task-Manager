952fe421a1f7e0d8d981e69036386ea1e51a67c6
S A Nahid <sanstudio120@gmail.com>
2026-04-21 22:02:33 +0600

    initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Task Manager',style: TextStyle(
+
+        ),),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+      ),
+
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/homepage.dart';
+import 'package:task_manager/screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      theme: ThemeData(
+
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
